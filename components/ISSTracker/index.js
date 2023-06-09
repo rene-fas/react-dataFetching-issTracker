e1d5c8e7fcc78d1d2f4baf3257035482099c5340
René Faßbender <rene-fassb@gmx.de>
2023-06-09 15:13:45 +0200

    feat: using swr for fetch

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -1,45 +1,44 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import useSWR from "swr";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
+function fetcher(url) {
+  return fetch(url).then((response) => {
+    if (!response.ok) {
+      const error = new Error("An error occurred while fetching the data.");
+      error.status = response.status;
+      throw error;
+    }
+    return response.json();
+  });
+}
+
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
+  const { data, error, mutate } = useSWR(URL, fetcher, {
+    refreshInterval: 5000,
   });
 
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
+  if (error) {
+    console.error(error);
   }
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
 
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
-      <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
-      />
+      {data ? (
+        <>
+          <Map longitude={data.longitude} latitude={data.latitude} />
+          <Controls
+            longitude={data.longitude}
+            latitude={data.latitude}
+            onRefresh={() => mutate()}
+          />
+        </>
+      ) : (
+        <p>No data yet... probably you will get them ...</p>
+      )}
     </main>
   );
 }
